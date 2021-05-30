344f3b9b70759229c69113d88f3f8ce1f1bd96ea
Aishwarya-Joshi11 <68340531+Aishwarya-Joshi11@users.noreply.github.com>
2021-05-30 16:06:27 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,18 @@
+# Add Implementations
+def add(x,y):
+    return x+y
+# Sub Implementation
+def subtract(x,y):
+    return x-y  #on aishwarya_joshi branch
+#Multiply Implemetation
+def multiply(x,y):
+    return x*y # on Bug456
+#Divide Implemetation
+def divide(x,y):
+    if y==0:
+	return DIVIDE_BY_O_ERROR
+    else:
+	return x/y      # on Bug456 branch
+# Sqauare Implemetation
+def square(x):
+    pass
