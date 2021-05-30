100b93ae652c0717b93a388db82ae52fa7aa8149
Aishwarya_joshi1 <aishwarya.joshi111298@gmail.com>
2021-05-30 16:41:54 +0530

    SUB functionn updated

--- math.py
+++ math.py
@@ -3,7 +3,7 @@ def add(x,y):
     return 1
 # Sub Implementation
 def subtract(x,y):
-    return x-y  #on aishwarya_joshi branch
+    return 2  #on aishwarya_joshi branch
 #Multiply Implemetation
 def multiply(x,y):
     return x*y # on Bug456
